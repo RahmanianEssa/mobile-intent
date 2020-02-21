8e7b132978f9cf66383966e69754b48c95065b4a
RahmanianEssa <rahmanianessa1@gmail.com>
2020-02-21 08:37:27 +0700

    parcelable

--- app/src/main/java/id/ac/polinema/intent/ParcelableActivity.java
+++ app/src/main/java/id/ac/polinema/intent/ParcelableActivity.java
@@ -1,15 +1,24 @@
 package id.ac.polinema.intent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ParcelableActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parcelable);
+
     }
 
+
+    public void handleSubmit2(View view) {
+
+    }
 }
